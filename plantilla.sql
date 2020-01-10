46b1e05cccff862622e880b73cdf30819643c560
Nashi91 <clabiancer1@educacion.navarra.es>
2020-01-10 15:37:59 +0100

    Fix'd formatting

--- plantilla.sql
+++ plantilla.sql
@@ -1,8 +1,10 @@
 /*
-        -----|NOMENCLATURAS|-----
+                -----|NOMENCLATURAS|-----
     SProc_???               ->  Procedimiento almacenado
     TRGHelper_???           ->  PA ejecutado por Desencadenador (TRG)
     [TrgInstead]            ->  ???
     [TrgAfter]              ->
     PrmKEY_tabla            ->  Restricción de clave primaria
     ExtKEY_tabla1_tabla2    ->  Restricción de clave externa
+
+*/
