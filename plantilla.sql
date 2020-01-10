53504b2dc2573c0b666722f6fc33a2e7c1c0e31d
Nashi91 <clabiancer1@educacion.navarra.es>
2020-01-10 15:36:19 +0100

    Added guidelines for naming standards

---
+++ plantilla.sql
@@ -0,0 +1,8 @@
+/*
+        -----|NOMENCLATURAS|-----
+    SProc_???               ->  Procedimiento almacenado
+    TRGHelper_???           ->  PA ejecutado por Desencadenador (TRG)
+    [TrgInstead]            ->  ???
+    [TrgAfter]              ->
+    PrmKEY_tabla            ->  Restricción de clave primaria
+    ExtKEY_tabla1_tabla2    ->  Restricción de clave externa
