3f08ac3ce11c9e2575e1f26ffff1cacd295ace6a
Nashi91 <clabiancer1@educacion.navarra.es>
2020-01-14 21:06:33 +0100

    Finished table declarations

--- DB_RTH.dev/db_rth_constructor.sql
+++ DB_RTH.dev/db_rth_constructor.sql
@@ -116,11 +116,6 @@ CREATE TABLE MENSAJES
 /*
     LARP-SIDE TABLES
 */
-CREATE TABLE REINOS
-(
-
-)
-
 CREATE TABLE PERSONAJES
 (
     usuario NVARCHAR(15) NOT NULL,
@@ -134,10 +129,88 @@ CREATE TABLE PERSONAJES
     dinero INT,
     clase NVARCHAR(15),
     magia NVARCHAR(10),
-
+    reino NVARCHAR(35) NOT NULL, --ExtKEY_REINOS_PERSONAJES
+        CONSTRAINT PrmKEY_PERSONAJES PRIMARY KEY (usuario),
+        CONSTRAINT ExtKEY_USUARIOS_PERSONAJES FOREIGN KEY (usuario)
+            REFERENCES USUARIOS(usuario)
     /*  TODO
         -Constraint de genero
         -Constraint de raza, clase y magia (???)
         -Default dinero 100 oros
     */
 )
+
+CREATE TABLE REINOS
+(
+    nombre NVARCHAR(35) NOT NULL,
+    lema NVARCHAR(20),
+    capital NVARCHAR(35) NOT NULL, --ExtKEY_REINOS_TERRITORIOS_CAPITAL
+    descripcion NVARCHAR(MAX),
+    creado_por NVARCHAR(15) NOT NULL,
+    forma_gobierno NVARCHAR(30) NOT NULL,
+    rey NVARCHAR(15) NOT NULL,
+    legado NVARCHAR(15),
+    contacto NVARCHAR(50) NOT NULL,
+    religion NVARCHAR(15),
+    ejercito BIGINT,
+        CONSTRAINT PrmKEY_REINOS PRIMARY KEY (nombre),
+        CONSTRAINT ExtKEY_PERSONAJES_REINOS FOREIGN KEY (creado_por)
+            REFERENCES USUARIOS(usuario),
+        CONSTRAINT ExtKEY_REINOS_PERSONAJES_REY FOREIGN KEY (rey)
+            REFERENCES PERSONAJES(usuario),
+        CONSTRAINT ExtKEY_REINOS_PERSNAJES_LEGADO FOREIGN KEY (legado)
+            REFERENCES PERSONAJES(usuario)
+    /* TODO
+        - Constraint de forma_gobierno (?¿)
+    */
+)
+    CREATE TABLE TERRITORIOS
+    (
+        nombre NVARCHAR(35) NOT NULL,
+        topologia NVARCHAR(15) NOT NULL,
+        reino NVARCHAR(25),
+        comida INT,
+        madera INT,
+        piedra INT,
+        hierro INT,
+        dinero INT,
+        edificios SMALLINT,
+        poblacion INT,
+        def TINYINT,
+        atk TINYINT,
+            CONSTRAINT PrmKEY_TERRITORIOS PRIMARY KEY (nombre),
+            CONSTRAINT ExtKEY_REINOS_TERRITORIOS FOREIGN KEY (reino)
+                REFERENCES REINOS(nombre)
+        /* TODO
+            -Constraint de topologia
+        */
+    )
+/*
+    EVENT-SIDE TABLES
+*/
+CREATE TABLE EVENTOS
+(
+    codevnt INT IDENTITY(1,1) NOT NULL,
+    nombre NVARCHAR(35) NOT NULL,
+    ubicacion NVARCHAR(40),
+    detalles NVARCHAR(MAX),
+    coste MONEY,
+    fecha_inicio DATETIME NOT NULL,
+    fecha_fin DATETIME,
+        CONSTRAINT PrmKEY_EVENTOS PRIMARY KEY (codevnt)
+    /* TODO
+        -Defaults de fecha
+        -Constraint de fechas inicio - fin
+    */
+)
+    CREATE TABLE PARTICIPANTES
+    (
+        usuario NVARCHAR(15) NOT NULL,
+        codevnt INT NOT NULL,
+        personaje BIT NOT NULL, -- no utiliza(0), si utiliza(1)
+            CONSTRAINT PrmKEY_PARTICIPANTES PRIMARY KEY (usuario),
+            CONSTRAINT ExtKEY_USUARIOS_PARTICIPANTES FOREIGN KEY (usuario)
+                REFERENCES USUARIOS(usuario),
+            CONSTRAINT ExtKEY_EVENTOS_PARTICIPANTES FOREIGN KEY (codevnt)
+                REFERENCES EVENTOS(codevnt)
+    )
