bf4f374b8ace1f125e7a2b1849c4d6c92ce68e26
Nashi91 <clabiancer1@educacion.navarra.es>
2020-01-21 13:45:44 +0100

    Adde template for triggers

--- plantilla.sql
+++ plantilla.sql
@@ -2,8 +2,8 @@
                 -----|NOMENCLATURAS|-----
     SProc_???               ->  Procedimiento almacenado
     TRGHelper_???           ->  PA ejecutado por Desencadenador (TRG)
-    [TrgInstead]            ->  ???
-    [TrgAfter]              ->
+    TRGIn_???               ->  Desencadenador de tipo INSTEAD
+    TRGAf_???               ->  Desencadenador de tipo AFTER
     Chk_campo               ->  Restriccion común
     PrmKEY_tabla            ->  Restricción de clave primaria
     ExtKEY_tabla1_tablaN    ->  Restricción de clave externa
