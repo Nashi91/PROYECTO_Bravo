519239ae0881c44eb60b82ea285599f994169d43
Nashi91 <clabiancer1@educacion.navarra.es>
2020-01-21 13:41:57 +0100

    Initial commit

---
+++ DB_RTH.dev/db_rth_DelAllUserData.sql
@@ -0,0 +1,8 @@
+/*----------------------------------------------
+    SP+TRG DelAllUserData | Reinos de Thalesia
+        Carlos Labiano Cerón / 2º GFS ASIR
+          Centro integrado Cuatrovientos
+                        v0.1
+                     21/01/2020
+ ----------------------------------------------*/
+ 
