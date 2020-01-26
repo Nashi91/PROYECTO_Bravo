d0b23718b58e5cb9e140269d694ab9524764dbed
Nashi91 <clabiancer1@educacion.navarra.es>
2020-01-26 15:03:52 +0100

    Merged with Mario-Cris SQL files and various fixes

--- DB_RTH.dev/db_rth_constructor.sql
+++ DB_RTH.dev/db_rth_constructor.sql
@@ -255,3 +255,228 @@ EXEC SP_BINDEFAULT DATE_TODAY, 'EVENTOS.fecha_inicio'
 EXEC SP_BINDEFAULT BITFALSE, 'USUARIOS.tipo'
 EXEC SP_BINDEFAULT BITFALSE, 'CONVERSACIONES.bloqueado'
 EXEC SP_BINDEFAULT BITFALSE, 'PARTICIPANTES.personaje'
+GO
+-- TRIGGER CREATION
+/*
+    --- [Mario] ---
+*/
+--REGLA 1:
+
+CREATE TRIGGER REGLA1 ON TEMAS
+INSTEAD OF INSERT
+AS
+	IF (SELECT TIPO FROM USUARIOS WHERE usuario=(SELECT creado_por FROM inserted))=2
+		BEGIN
+			INSERT INTO TEMAS
+			SELECT * FROM inserted
+		END
+	ELSE
+		PRINT 'NO PUEDES CREAR UN TEMA PORQUE NO ERES ADMINISTRADOR'
+GO
+/*
+EXPLICACIÓN: en este caso necesitamos limitar el permiso de creación de temas a los usuarios que sean administradores,
+para ello mediante un trigger que se lanzará tras intentar añadir registros a temas, comprobaremos que dicho usuario
+que intenta crear un nuevo tema "sea de tipo administrador", si lo es se realiza la operación pero si no se envía un 
+mensaje de error.
+*/
+--REGLA 2:
+
+CREATE TRIGGER REGLA2 ON MODERADORES
+INSTEAD OF INSERT
+AS
+	IF (SELECT TIPO FROM USUARIOS WHERE usuario=(SELECT usuario FROM inserted))>=1
+		BEGIN
+			INSERT INTO MODERADORES
+			SELECT * FROM inserted
+		END
+	ELSE
+		PRINT 'NO PUEDES SER AÑADIDO A LOS MODERADORES PORQUE NO ERES MODERADOR'
+GO
+/*
+EXPLICACIÓN: en este caso necesitamos limitar el permiso de moderación de temas a los usuarios que sean moderadores,
+un moderador sólo puede moderar un tema si se encuentra en la tabla de moderadores, pero para estar en esa tabla debe 
+ser moderador, con lo cual mediante un trigger que se lanzará tras intentar añadir registros a moderadores, 
+comprobaremos que dicho usuario se intenta añadir a los moderadores "sea de tipo moderador", si lo es se realiza la 
+operación pero si no se envía un mensaje de error.
+*/
+--Regla 3: El codigo de las conversaciones sera igual al numero de la conversacion dentro de su correspondiente tema. [TRGHelper_GenWeakCode]
+
+--Por optimización se asume que el procedimiento ya existe previamente a la ejecución del trigger.
+/*CREATE PROCEDURE CODCONVERS
+@CODTEMA CHAR(4)
+AS
+	DECLARE @CODCONV INT
+	SET @CODCONV=(SELECT COUNT(codtema) FROM CONVERSACIONES WHERE codtema=@CODTEMA)+1
+	--En caso de no haber conversaciones la cuenta de codtema dentro de conversaciones daría 0, luego sumaríamos 1.
+
+	RETURN @CODCONV
+
+CREATE TRIGGER REGLA3 ON CONVERSACIONES
+INSTEAD OF INSERT
+AS
+	BEGIN
+--@TITULO NVARCHAR(30),
+--@CREADOR NVARCHAR(15),
+--@FECHACREA DATETIME,
+--@BLOQUEADO INT
+		EXEC CODCONVERS ''
+		INSERT INTO CONVERSACIONES
+		SELECT * FROM inserted
+		
+		--INSERT INTO CONVERSACIONES (codconv,codtema,titulo,creado_por,fecha_creacion,bloqueado)
+		--VALUES (@CODCONV,@CODTEMA,@TITULO,@CREADOR,@FECHACREA,@BLOQUEADO)
+	END*/
+/*
+EXPLICACIÓN:
+*/
+/*
+--Regla 4: El codigo de los mensajes sera igual al numero del mensaje dentro de su correspondiente conversacion.      [^]
+
+CREATE PROCEDURE CODMSJS
+AS
+	INSERT INTO MENSAJES
+	--SELECT * FROM MENSAJES WHERE codmsg=(SELECT COUNT(codconv) FROM MENSAJES WHERE codconv=(SELECT codconv FROM CONVERSACIONES))+1
+
+CREATE TRIGGER REGLA4 ON MENSAJES
+AFTER INSERT
+	BEGIN
+		EXEC CODMSJS
+	END*/
+/*
+EXPLICACIÓN:
+*/
+/*
+    --- [Cristian] ---
+*/
+-- REGLA 5: En caso de que un personaje muera, debera de ser eliminado del registro y almacenarse en un registro a parte. [TRGHelper_PlayerCharDeath]
+CREATE TRIGGER TRGHelper_PlayerCharDeath ON PERSONAJES
+INSTEAD OF DELETE
+AS
+-- Se hace una comprobación de si el personaje existe en dicha tabla.
+	IF EXISTS (SELECT * FROM PERSONAJES WHERE PERSONAJES.USUARIO = (SELECT USUARIO FROM DELETED))
+	    PRINT 'ERROR EL PERSONAJE NO EXISTE EN LA TABLA :V'
+	ELSE
+--Si la tabla no existe se crea otra donde se insertan los personajes muertos.
+    BEGIN
+	    IF NOT EXISTS (SELECT * FROM SYSOBJECTS WHERE NAME = 'DEAD_CHARACTERS')
+	    	CREATE TABLE DEAD_CHARACTERS(
+	    	usuario NVARCHAR(15),
+	    	nombre NVARCHAR(30),
+	    	apellido1 NVARCHAR(30),
+	    	apellido2 NVARCHAR(30),
+	    	raza NVARCHAR(15),
+	    	genero VARCHAR(9),
+	    	religion NVARCHAR(15),
+	    	historia NVARCHAR(MAX),
+	    	dinero INT,
+	    	clase NVARCHAR(15),
+	    	magia NVARCHAR(10),
+	    	reino NVARCHAR(35)
+	    	)
+
+	    	INSERT INTO DEAD_CHARACTERS
+	    	SELECT * FROM PERSONAJES
+	    	    WHERE PERSONAJES.USUARIO IN (SELECT USUARIO FROM DELETED)
+-- En el caso de existir un rey o legado, con esto comprobamos de que existe tal rey o legado.
+
+	    IF EXISTS (SELECT REY FROM REINOS WHERE REINOS.REY = (SELECT USUARIO FROM DELETED))
+	    	EXEC SProc_RulerDeath (SELECT USUARIO FROM DELETED)
+    
+	    IF EXISTS (SELECT LEGADO FROM REINOS WHERE REINOS.LEGADO = (SELECT USUARIO FROM DELETED))	
+	    BEGIN
+	    	UPDATE REINOS
+	    	SET LEGADO = NULL
+	    	WHERE REINOS.LEGADO = (SELECT LEGADO FROM DELETED)
+	    END
+    
+	    DELETE PERSONAJES -- Hacemos su correspondiente eliminación.
+	    FROM PERSONAJES
+	    WHERE PERSONAJES.USUARIO IN (SELECT USUARIO FROM DELETED)
+    END
+GO
+-- REGLA 10: La fecha de fin de un evento debera ser siempre posterior respetando un minimo de un dia respecto a la fecha de inicio
+CREATE TRIGGER EVENTS_DATES ON EVENTOS
+INSTEAD OF UPDATE
+AS
+	IF (SELECT FECHA_FIN FROM DELETED) = (SELECT FECHA_FIN FROM INSERTED) AND 
+        (SELECT FECHA_INICIO FROM DELETED) = (SELECT FECHA_INICIO FROM INSERTED) -- Comprobar que no se ha modificado nada para poder actualizar.
+	    BEGIN
+	    	UPDATE EVENTOS
+	    	SET CODEVNT = (SELECT CODEVNT FROM INSERTED),
+	    	    NOMBRE = (SELECT NOMBRE FROM INSERTED),
+	    	    UBICACION = (SELECT UBICACION FROM INSERTED),
+	    	    DETALLES = (SELECT DETALLES FROM INSERTED),
+	    	    COSTE = (SELECT COSTE FROM INSERTED),
+	    	    FECHA_INICIO = (SELECT FECHA_INICIO FROM INSERTED),
+	    	    FECHA_FIN = (SELECT FECHA_FIN FROM INSERTED)
+	    	WHERE CODEVNT = (SELECT CODEVNT FROM DELETED)
+	    END
+	ELSE IF DATEDIFF(day, (SELECT FECHA_INICIO FROM INSERTED), (SELECT FECHA_FIN FROM INSERTED)) >= 1
+        BEGIN
+            UPDATE EVENTOS
+	    	SET CODEVNT = (SELECT CODEVNT FROM INSERTED),
+	    	    NOMBRE = (SELECT NOMBRE FROM INSERTED),
+	    	    UBICACION = (SELECT UBICACION FROM INSERTED),
+	    	    DETALLES = (SELECT DETALLES FROM INSERTED),
+	    	    COSTE = (SELECT COSTE FROM INSERTED),
+	    	    FECHA_INICIO = (SELECT FECHA_INICIO FROM INSERTED),
+	    	    FECHA_FIN = (SELECT FECHA_FIN FROM INSERTED)
+	    	WHERE CODEVNT = (SELECT CODEVNT FROM DELETED)
+        END
+    ELSE
+        PRINT 'LA DIFERENCIA ENTRE LA FECHA DE INICIO Y LA FECHA DE FIN ES MENOR DE 1 DIA'
+GO
+-- REGLA 13: Los reinos unicamente podran ser declarados por usuarios administradores
+CREATE TRIGGER KINGDOM_ROOTS ON REINOS
+INSTEAD OF INSERT
+-- Hacemos una comprobación de que el usuario sea root o no... Si no lo es no puede declarar los reinos
+AS
+	IF (SELECT TIPO FROM USUARIOS WHERE USUARIOS.USUARIO=(SELECT CREADO_POR FROM INSERTED))<>2
+	    BEGIN
+	    	PRINT 'ERROR, EL USUARIO NO ES ROOT :V'
+	    END
+	ELSE
+	    BEGIN
+	    	INSERT INTO REINOS
+	    	SELECT * FROM INSERTED
+	    END
+GO
+-- STORED PROCEDURE CREATION
+/*
+    --- [Cristian] ---
+*/
+--REGLA 6: Dada la destruccion de un reino, todos sus territorios deberan volverse tierra de nadie. [SProc_KngdmDestroyed]
+CREATE PROCEDURE SProc_KngdmDestroyed
+@KINGDOM_DEAD NVARCHAR(35)
+AS
+	UPDATE TERRITORIOS
+	    SET REINO = NULL
+	WHERE TERRITORIOS.REINO = @KINGDOM_DEAD
+GO
+/*
+    --- [Mario] ---
+*/
+--REGLA 15:
+
+CREATE PROCEDURE [SProc_RulerDeath] 
+@OLDKING NVARCHAR(15)
+AS
+	DECLARE @NEWKING NVARCHAR(15)
+	SET @NEWKING=(SELECT legado FROM REINOS WHERE rey=@OLDKING)
+	
+	UPDATE REINOS
+	    SET rey=legado
+	WHERE rey=@OLDKING
+
+	UPDATE REINOS
+	    SET legado=NULL
+	WHERE rey=@NEWKING
+-- EXEC [SProc_RulerDeath] ''
+/*
+EXPLICACIÓN: en este caso necesitamos un procedimiento que "elimine" el rey caído y lo sustituya por su 
+correspondiente legado, para ello declaramos una variable en la que se almacenará el rey a sustituir y 
+otra variable en la que almacenaremos el nuevo rey (legado), mediante el procedimiento modificaremos la 
+tabla de reinos sustituyendo el rey cuyo nombre coincida con el introducido (OLDKING) por el personaje sea 
+el legado en ese momento (mismo registro) y mediante otra modificación dejaremos el puesto de legado como 
+nulo, es decir, libre donde el rey sea igual al nuevo rey (el antiguo legado).
+*/
