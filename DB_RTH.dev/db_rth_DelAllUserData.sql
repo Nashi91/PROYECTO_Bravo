84ed3b1a43edd8cc619ceb0e5cf0e66bcb61b296
Nashi91 <clabiancer1@educacion.navarra.es>
2020-01-28 20:24:58 +0100

    Final rev. (Release)

--- DB_RTH.dev/db_rth_DelAllUserData.sql
+++ DB_RTH.dev/db_rth_DelAllUserData.sql
@@ -2,8 +2,8 @@
     SP+TRG DelAllUserData | Reinos de Thalesia
         Carlos Labiano Cerón / 2º GFS ASIR
           Centro integrado Cuatrovientos
-                        v1.2
-                     27/01/2020
+                  v1.3 Relase Ver.
+                     28/01/2020
  ----------------------------------------------*/
 -- INIT DATABASE
 USE db_rth
